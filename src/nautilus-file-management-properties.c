f408bb975740cd50c375893bc96a4758d083d071
William Jon McCann <jmccann@redhat.com>
2012-08-14 14:10:28 -0400

    Remove the open folders in new windows option
    This is not something we want to encourage as it basically
    breaks the browser model.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=681919

--- src/nautilus-file-management-properties.c
+++ src/nautilus-file-management-properties.c
@@ -54,7 +54,6 @@
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_FOLDERS_FIRST_WIDGET "sort_folders_first_checkbutton"
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_LABELS_BESIDE_ICONS_WIDGET "labels_beside_icons_checkbutton"
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_ALL_COLUMNS_SAME_WIDTH "all_columns_same_width_checkbutton"
-#define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_ALWAYS_USE_BROWSER_WIDGET "always_use_browser_checkbutton"
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_TRASH_CONFIRM_WIDGET "trash_confirm_checkbutton"
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_TRASH_DELETE_WIDGET "trash_delete_checkbutton"
 #define NAUTILUS_FILE_MANAGEMENT_PROPERTIES_OPEN_NEW_WINDOW_WIDGET "new_window_checkbutton"
@@ -532,17 +531,6 @@ bind_builder_bool (GtkBuilder *builder,
 			 "active", G_SETTINGS_BIND_DEFAULT);
 }
 
-static void
-bind_builder_bool_inverted (GtkBuilder *builder,
-			    GSettings *settings,
-			    const char *widget_name,
-			    const char *prefs)
-{
-	g_settings_bind (settings, prefs,
-			 gtk_builder_get_object (builder, widget_name),
-			 "active", G_SETTINGS_BIND_INVERT_BOOLEAN);
-}
-
 static gboolean
 enum_get_mapping (GValue             *value,
 		  GVariant           *variant,
@@ -728,9 +716,6 @@ nautilus_file_management_properties_dialog_setup (GtkBuilder *builder, GtkWindow
 	bind_builder_bool (builder, nautilus_preferences,
 			   NAUTILUS_FILE_MANAGEMENT_PROPERTIES_FOLDERS_FIRST_WIDGET,
 			   NAUTILUS_PREFERENCES_SORT_DIRECTORIES_FIRST);
-	bind_builder_bool_inverted (builder, nautilus_preferences,
-				    NAUTILUS_FILE_MANAGEMENT_PROPERTIES_ALWAYS_USE_BROWSER_WIDGET,
-				    NAUTILUS_PREFERENCES_ALWAYS_USE_BROWSER);
 
 	bind_builder_bool (builder, nautilus_preferences,
 			   NAUTILUS_FILE_MANAGEMENT_PROPERTIES_TRASH_CONFIRM_WIDGET,
