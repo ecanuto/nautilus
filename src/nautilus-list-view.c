7fbe52f23b2518d7b49b167d7896c664fadbfecc
William Jon McCann <jmccann@redhat.com>
2012-08-26 10:36:42 -0400

    Rename entire folder name
    Not just the part before the "."
    
    https://bugzilla.gnome.org/show_bug.cgi?id=664266

--- src/nautilus-list-view.c
+++ src/nautilus-list-view.c
@@ -2683,7 +2683,6 @@ nautilus_list_view_start_renaming_file (NautilusView *view,
 	NautilusListView *list_view;
 	GtkTreeIter iter;
 	GtkTreePath *path;
-	gint start_offset, end_offset;
 	
 	list_view = NAUTILUS_LIST_VIEW (view);
 	
@@ -2724,8 +2723,13 @@ nautilus_list_view_start_renaming_file (NautilusView *view,
 
 	/* set cursor also triggers editing-started, where we save the editable widget */
 	if (list_view->details->editable_widget != NULL) {
-		eel_filename_get_rename_region (list_view->details->original_name,
-						&start_offset, &end_offset);
+		int start_offset = 0;
+		int end_offset = -1;
+
+		if (!select_all) {
+			eel_filename_get_rename_region (list_view->details->original_name,
+							&start_offset, &end_offset);
+		}
 
 		gtk_editable_select_region (GTK_EDITABLE (list_view->details->editable_widget),
 					    start_offset, end_offset);
